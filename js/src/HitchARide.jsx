26fcc5e42b9657ac399a976eaa302b64202567b2
Peter Donker <peter@bring2mind.net>
2016-05-18 12:55:31 +0200

    Step 3: Organize your code and split things off into their own files

--- js/src/HitchARide.jsx
+++ js/src/HitchARide.jsx
@@ -1,27 +1,4 @@
-var MyComponent = React.createClass({
-
-  getInitialState() {
-    return {
-      name: 'DNN Connect'
-    }
-  },
-
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.state.name}!</h1>
-        <p><input type="text" ref="txtName" value={this.state.name} onChange={this.textChanged} /></p>
-      </div>
-    );
-  },
-
-  textChanged() {
-    this.setState({
-      name: this.refs.txtName.value
-    });
-  }
-
-});
+var MyComponent = require('./MyComponent.jsx');
 
 (function($, window, document, undefined) {
 
---
+++ js/src/MyComponent.jsx
@@ -0,0 +1,24 @@
+module.exports = React.createClass({
+
+  getInitialState() {
+    return {
+      name: 'DNN Connect'
+    }
+  },
+
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.name}!</h1>
+        <p><input type="text" ref="txtName" value={this.state.name} onChange={this.textChanged} /></p>
+      </div>
+    );
+  },
+
+  textChanged() {
+    this.setState({
+      name: this.refs.txtName.value
+    });
+  }
+
+});
